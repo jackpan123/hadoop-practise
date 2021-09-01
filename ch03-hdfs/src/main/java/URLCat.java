2b5f139110a24a554dcb1a943b4e4d24a4a66173
jackpan <826647196@qq.com>
2021-09-01 22:14:20 +0800

    HDFS url cat

---
+++ ch03-hdfs/src/main/java/URLCat.java
@@ -0,0 +1,25 @@
+import java.io.InputStream;
+import java.net.URL;
+import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
+import org.apache.hadoop.io.IOUtils;
+
+/**
+ * @author jackpan
+ * @version v1.0 2021/9/1 22:08
+ */
+public class URLCat {
+
+    static {
+        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+    }
+
+    public static void main(String[] args) throws Exception {
+        InputStream in = null;
+        try {
+            in = new URL("hdfs://localhost/user/jackpan/quangle.txt").openStream();
+            IOUtils.copyBytes(in, System.out, 4096, false);
+        } finally {
+            IOUtils.closeStream(in);
+        }
+    }
+}
