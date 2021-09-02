1ef5078ba36166348c622639a96db74746c00ff0
jackpan <826647196@qq.com>
2021-09-02 13:11:40 +0800

    Example 3-2

--- ch03-hdfs/src/main/java/FileSystemCat.java
+++ ch03-hdfs/src/main/java/FileSystemCat.java
@@ -1,23 +1,25 @@
 import java.io.InputStream;
+import java.net.URI;
 import java.net.URL;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
 
 /**
  * @author jackpan
  * @version v1.0 2021/9/1 22:08
  */
-public class URLCat {
-
-    static {
-        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
-    }
+public class FileSystemCat {
 
     public static void main(String[] args) throws Exception {
+        String uri = args[0];
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(URI.create(uri), conf);
         InputStream in = null;
         try {
-//            in = new URL("hdfs://localhost/user/jackpan/quangle.txt").openStream();
-            in = new URL(args[0]).openStream();
+            in = fs.open(new Path(uri));
             IOUtils.copyBytes(in, System.out, 4096, false);
         } finally {
             IOUtils.closeStream(in);
