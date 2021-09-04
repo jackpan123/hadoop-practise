37b1290b3249598f75f17c9606027ef7ab46fcef
jackpan <826647196@qq.com>
2021-09-04 10:05:35 +0800

    Create RegexExcludePathFilter.java

---
+++ ch03-hdfs/src/main/java/RegexExcludePathFilter.java
@@ -0,0 +1,19 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.PathFilter;
+
+/**
+ * @author jackpan
+ * @version v1.0 2021/9/4 10:03
+ */
+public class RegexExcludePathFilter implements PathFilter {
+
+    private final String regex;
+
+    public RegexExcludePathFilter(String regex) {
+        this.regex = regex;
+    }
+    @Override
+    public boolean accept(Path path) {
+        return !path.toString().matches(regex);
+    }
+}
