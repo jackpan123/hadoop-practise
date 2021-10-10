5df246a774d817b3dcec2feee2abf71c7280b7a0
jackpan <826647196@qq.com>
2021-10-10 10:41:04 +0800

    MaxTemperatureUsingSecondarySort

--- ch09-mr-features/src/main/java/MaxTemperatureUsingSecondarySort.java
+++ ch09-mr-features/src/main/java/MaxTemperatureUsingSecondarySort.java
@@ -18,8 +18,90 @@ import org.apache.hadoop.util.ToolRunner;
  */
 public class MaxTemperatureUsingSecondarySort extends Configured implements Tool {
 
+    static class MaxTemperatureMapper
+        extends Mapper<LongWritable, Text, IntPair, NullWritable> {
+
+        private NcdcRecordParser parser = new NcdcRecordParser();
+
+        @Override
+        protected void map(LongWritable key, Text value,
+            Context context) throws IOException, InterruptedException {
+
+            parser.parse(value);
+            if (parser.isValidTemperature()) {
+                /*[*/context.write(new IntPair(parser.getYearInt(),
+                    parser.getAirTemperature()), NullWritable.get());/*]*/
+            }
+        }
+    }
+
+    static class MaxTemperatureReducer extends Reducer<IntPair, NullWritable, IntPair, NullWritable> {
+
+        @Override
+        protected void reduce(IntPair key, Iterable<NullWritable> values, Context context)
+            throws IOException, InterruptedException {
+            context.write(key, NullWritable.get());
+        }
+    }
+
+    public static class FirstPartitioner
+        extends Partitioner<IntPair, NullWritable> {
+
+        @Override
+        public int getPartition(IntPair key, NullWritable value, int numPartitions) {
+            return Math.abs(key.getFirst() * 127) % numPartitions;
+        }
+    }
+
+    public static class KeyComparator extends WritableComparator {
+        protected KeyComparator() {
+            super(IntPair.class, true);
+        }
+
+        @Override
+        public int compare(WritableComparable w1, WritableComparable w2) {
+            IntPair ip1 = (IntPair) w1;
+            IntPair ip2 = (IntPair) w2;
+            int cmp = IntPair.compare(ip1.getFirst(), ip2.getSecond());
+            if (cmp != 0) {
+                return cmp;
+            }
+            return -IntPair.compare(ip1.getSecond(), ip2.getSecond());
+        }
+    }
+
+    public static class GroupComparator extends WritableComparator {
+        protected GroupComparator() {
+            super(IntPair.class, true);
+        }
+        @Override
+        public int compare(WritableComparable w1, WritableComparable w2) {
+            IntPair ip1 = (IntPair) w1;
+            IntPair ip2 = (IntPair) w2;
+            return IntPair.compare(ip1.getFirst(), ip2.getFirst());
+        }
+    }
+
     @Override
     public int run(String[] args) throws Exception {
-        return 0;
+        Job job = JobBuilder.parseInputAndOutput(this, getConf(), args);
+        if (job == null) {
+            return -1;
+        }
+
+        job.setMapperClass(MaxTemperatureMapper.class);
+        job.setPartitionerClass(FirstPartitioner.class);
+        job.setSortComparatorClass(KeyComparator.class);
+        job.setGroupingComparatorClass(GroupComparator.class);
+        job.setReducerClass(MaxTemperatureReducer.class);
+        job.setOutputKeyClass(IntPair.class);
+        job.setOutputValueClass(NullWritable.class);
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new MaxTemperatureUsingSecondarySort(), args);
+        System.exit(exitCode);
     }
 }
