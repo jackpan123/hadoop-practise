2f1c2400290b762a24f3b1ff19dcf03b8536caed
jackpan <826647196@qq.com>
2022-06-11 22:30:10 +0800

    Update FileCopyWithProgress.java

--- ch03-hdfs/src/main/java/FileCopyWithProgress.java
+++ ch03-hdfs/src/main/java/FileCopyWithProgress.java
@@ -20,6 +20,7 @@ public class FileCopyWithProgress {
         String localSrc = args[0];
         String dst = args[1];
         InputStream in = new BufferedInputStream(new FileInputStream(localSrc));
+
         Configuration conf = new Configuration();
         FileSystem fs = FileSystem.get(URI.create(dst), conf);
         OutputStream out = fs.create(new Path(dst), new Progressable() {
@@ -28,6 +29,7 @@ public class FileCopyWithProgress {
                 System.out.print(".");
             }
         });
+
         IOUtils.copyBytes(in, out, 4096, true);
     }
 }
