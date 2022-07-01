2229be1a80367989d23576b122ee58e564d6e016
jackpan <826647196@qq.com>
2022-07-01 13:16:43 +0800

    Update MaxTemperatureMapperTest.java

--- ch06-mr-dev/src/test/java/v1/MaxTemperatureMapperTest.java
+++ ch06-mr-dev/src/test/java/v1/MaxTemperatureMapperTest.java
@@ -18,15 +18,14 @@ public class MaxTemperatureMapperTest {
 
     @Test
     public void processesValidRecord() throws IOException, InterruptedException {
-        Text value = new Text("0043011990999991950051518004+68750+023550FM-12+0382" +
-            // Year ^^^^
-            "99999V0203201N00261220001CN9999999N9-00111+99999999999");
-        // Temperature ^^^^^
+
+        Text value = new Text("0043011990999991950051518004+68750+023550FM-12+038299999V0203201N00261220001CN9999999N9-00111+99999999999");
+
         new MapDriver<LongWritable, Text, Text, IntWritable>()
-            .withMapper(new MaxTemperatureMapper())
-            .withInput(new LongWritable(0), value)
-            .withOutput(new Text("1950"), new IntWritable(-11))
-            .runTest();
+                .withMapper(new MaxTemperatureMapper())
+                .withInput(new LongWritable(0), value)
+                .withOutput(new Text("1950"), new IntWritable(-11))
+                .runTest();
     }
 
     @Ignore // since we are showing a failing test in the book
