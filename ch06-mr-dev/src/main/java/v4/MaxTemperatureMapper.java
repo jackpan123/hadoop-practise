c67c68a2d265630f87b3a980a0f19436a3158e3e
jackpan <826647196@qq.com>
2022-07-02 08:48:52 +0800

    Update MaxTemperatureMapper.java

--- ch06-mr-dev/src/main/java/v4/MaxTemperatureMapper.java
+++ ch06-mr-dev/src/main/java/v4/MaxTemperatureMapper.java
@@ -2,33 +2,38 @@ package v4;
 // cc MaxTemperatureMapperV4 Mapper for maximum temperature example
 
 import java.io.IOException;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 // vv MaxTemperatureMapperV4
+
+/**
+ * MaxTemperatureMapper V4
+ * @author jackpan
+ */
 public class MaxTemperatureMapper
-  extends Mapper<LongWritable, Text, Text, IntWritable> {
+        extends Mapper<LongWritable, Text, Text, IntWritable> {
 
-  enum Temperature {
-    MALFORMED
-  }
-
-  private NcdcRecordParser parser = new NcdcRecordParser();
-
-  @Override
-  public void map(LongWritable key, Text value, Context context)
-      throws IOException, InterruptedException {
-
-    parser.parse(value);
-    if (parser.isValidTemperature()) {
-      int airTemperature = parser.getAirTemperature();
-      context.write(new Text(parser.getYear()), new IntWritable(airTemperature));
-    } else if (parser.isMalformedTemperature()) {
-      System.err.println("Ignoring possibly corrupt input: " + value);
-      context.getCounter(Temperature.MALFORMED).increment(1);
+    enum Temperature {
+        MALFORMED
     }
-  }
+
+    NcdcRecordParser parser = new NcdcRecordParser();
+
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        parser.parse(value);
+        if (parser.isValidTemperature()) {
+            int airTemperature = parser.getAirTemperature();
+
+            context.write(new Text(parser.getYear()), new IntWritable(airTemperature));
+        } else if (parser.isMalformedTemperature()) {
+            System.err.println("Ignoring possibly corrupt input: " + value);
+            context.getCounter(Temperature.MALFORMED).increment(1L);
+        }
+    }
 }
-// ^^ MaxTemperatureMapperV4
