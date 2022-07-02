f75911ca3dd20a5336431b21949f0298d67de20c
jackpan <826647196@qq.com>
2022-07-02 08:31:03 +0800

    Update MaxTemperatureMapper.java

--- ch06-mr-dev/src/main/java/v3/MaxTemperatureMapper.java
+++ ch06-mr-dev/src/main/java/v3/MaxTemperatureMapper.java
@@ -2,37 +2,44 @@
 package v3;
 
 import java.io.IOException;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import v2.NcdcRecordParser;
 
-//vv MaxTemperatureMapperV3
+/**
+ * MaxTemperatureMapper v3
+ *
+ * @author jackpan
+ */
 public class MaxTemperatureMapper
-  extends Mapper<LongWritable, Text, Text, IntWritable> {
+        extends Mapper<LongWritable, Text, Text, IntWritable> {
 
-  /*[*/enum Temperature {
-    OVER_100
-  }/*]*/
-
-  private NcdcRecordParser parser = new NcdcRecordParser();
-
-  @Override
-  public void map(LongWritable key, Text value, Context context)
-      throws IOException, InterruptedException {
-
-    parser.parse(value);
-    if (parser.isValidTemperature()) {
-      int airTemperature = parser.getAirTemperature();
-      /*[*/if (airTemperature > 1000) {
-        System.err.println("Temperature over 100 degrees for input: " + value);
-        context.setStatus("Detected possibly corrupt record: see logs.");
-        context.getCounter(Temperature.OVER_100).increment(1);
-      }/*]*/
-      context.write(new Text(parser.getYear()), new IntWritable(airTemperature));
+    enum Temperature {
+        OVER_100
     }
-  }
+
+    private NcdcRecordParser parser = new NcdcRecordParser();
+
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        parser.parse(value);
+        if (parser.isValidTemperature()) {
+            int airTemperature = parser.getAirTemperature();
+            if (airTemperature > 1000) {
+                System.err.println("Temperature over 100 degrees for input: " + value);
+                // Set map status, and you can go through log to find some details.
+                context.setStatus("Detected possibly corrupt record: see logs.");
+                context.getCounter(Temperature.OVER_100).increment(1);
+            }
+
+            context.write(new Text(parser.getYear()), new IntWritable(airTemperature));
+        }
+
+    }
+
 }
-//^^ MaxTemperatureMapperV3
 
