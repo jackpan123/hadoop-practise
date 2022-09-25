7e1cd0e0d5aeb2ce77304532bcfdfb1fe6edd613
jackpan <826647196@qq.com>
2022-09-25 22:20:31 +0800

    add new project

---
+++ hadoop-springboot/src/test/java/com/woailqw/hadoopspringboot/HadoopSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woailqw.hadoopspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadoopSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
