b5e5be8776dd773935f3a09df3ff2c30dd33da12
jackpan <826647196@qq.com>
2022-10-04 23:32:44 +0800

    ConfigurationPrinter

--- ch06-mr-dev/src/main/java/com/jackpan/hadoop/mrdev/ConfigurationPrinter.java
+++ ch06-mr-dev/src/main/java/com/jackpan/hadoop/mrdev/ConfigurationPrinter.java
@@ -1,28 +1,37 @@
-import java.util.Map.Entry;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.util.*;
+package com.jackpan.hadoop.mrdev;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.util.Map;
 
 /**
- * Get hadoop configuration.
- * @author jackpan
- * @version v1.0 2021/9/23 12:44
- */
+ * ConfigurationPrinter操作类
+ *
+ * @author JackPan
+ * @date 2022/10/04 23:22
+ **/
 public class ConfigurationPrinter extends Configured implements Tool {
+
+
     static {
-        Configuration.addDefaultResource("hdfs-default.xml");
-        Configuration.addDefaultResource("hdfs-site.xml");
-        Configuration.addDefaultResource("yarn-default.xml");
-        Configuration.addDefaultResource("yarn-site.xml");
+        Configuration.addDefaultResource("hadoop-default.xml");
+        Configuration.addDefaultResource("hadoop-site.xml");
         Configuration.addDefaultResource("mapred-default.xml");
         Configuration.addDefaultResource("mapred-site.xml");
+        Configuration.addDefaultResource("yarn-default.xml");
+        Configuration.addDefaultResource("yarn-site.xml");
     }
 
     @Override
     public int run(String[] args) throws Exception {
         Configuration conf = getConf();
-        for (Entry<String, String> entry: conf) {
+        for (Map.Entry<String, String> entry : conf) {
             System.out.printf("%s=%s\n", entry.getKey(), entry.getValue());
         }
+
         return 0;
     }
 
