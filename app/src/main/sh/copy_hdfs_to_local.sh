b860eb2f24ad0f18e96ce3088d5b02bb4a2ac22a
JackPan <826647196@qq.com>
2023-07-30 15:48:31 +0800

    Update copy_hdfs_to_local.sh

--- app/src/main/sh/copy_hdfs_to_local.sh
+++ app/src/main/sh/copy_hdfs_to_local.sh
@@ -2,6 +2,6 @@
 
 for i in {1903..1920};
 do
-  $HADOOP_HOME/bin/hadoop fs -copyToLocal /user/root/gz/home/jack-pan-loganalyzes/$i.gz /home/jack-pan-loganalyzes/all_year_gz_file/$i.gz
+  $HADOOP_HOME/bin/hadoop fs -copyToLocal /user/root/gz/home/jack-pan-loganalyzes/$i.gz /home/jack-pan-loganalyzes/all/$i.gz
   echo "reporter:status:Processed $i.gz" >&2
 done
